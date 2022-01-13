607cbfbf2e702c616bc7a6753c183dd92b9279cd
acidrixxxon <rizhenko.vadim.r1.fc@gmail.com>
2022-01-13 16:56:39 +0200

    закончил header + адаптив под все экраны

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+const navbar = document.querySelector('.navbar');
+
 const closeBtnHandler = () => {
-  document.querySelector('.header__navbar').classList.remove('show');
+  document.querySelector('.navbar').classList.remove('show');
 };
 
 const showNavbar = () => {
-  document.querySelector('.header__navbar').classList.add('show');
+  document.querySelector('.navbar').classList.add('show');
 };
 
-document
-  .querySelector('.header__navbar__buttons--closeBtn')
-  .addEventListener('click', closeBtnHandler);
+document.querySelector('.navbar__buttons--closeBtn').addEventListener('click', closeBtnHandler);
 document.querySelector('.header__nav__hamburger').addEventListener('click', showNavbar);
+
+// document.querySelector('#header').onClick((e) => {
+//   console.log(e);
+//   if (navbar.classList.contains('show')) {
+//     console.log(true);
+//     console.log(e.target);
+//     if (e.target !== navbar) {
+//       closeBtnHandler();
+//     }
+//   }
+// });
